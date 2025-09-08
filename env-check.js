8faa345cf551474c6bd6fbcf1ee77b45864c3418
Micael Pereira <pcmicas@gmail.com>
2025-09-08 07:16:31 -0300

    clena code

--- env-check.js
+++
@@ -1,6 +0,0 @@
-require('dotenv').config({ debug: true });
-console.log('ENCODING OK, VARS:', {
-  EMARSYS_USER: process.env.EMARSYS_USER,
-  EMARSYS_SECRET: process.env.EMARSYS_SECRET,
-  EMARSYS_BEARER_TOKEN: process.env.EMARSYS_BEARER_TOKEN?.slice(0, 6) + '...' // não loga tudo
-});
